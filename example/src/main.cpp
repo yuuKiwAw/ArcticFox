b5d6e3e8f23fd8c0d153693e7c407f4ab5dab398
yuuKiwAw <zhangtuo1999@gmail.com>
2023-03-11 14:15:17 +0800

    feat(example): update example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include "Application.hpp"
 
+ArcticFox::Application* app = new ArcticFox::Application();
+
+class ExampleLayer : public ArcticFox::Layer
+{
+public:
+	virtual void OnUIRender() override
+	{
+		ImGui::Begin("Hello ArcticFox");
+		if (ImGui::Button("Button")) {
+
+        }
+		ImGui::End();
+	}
+};
+
+class ExampleLayer2 : public ArcticFox::Layer
+{
+public:
+	virtual void OnUIRender() override
+	{
+		ImGui::Begin("Hello ArcticFox2");
+		if (ImGui::Button("Button")) {
+
+        }
+		ImGui::End();
+	}
+};
+
 int main(int argc, char** argv) {
-    ArcticFox::Application* app = new ArcticFox::Application();
+    app->PushLayer<ExampleLayer>();
+    app->PushLayer<ExampleLayer2>();
     app->Run();
     delete app;
 
