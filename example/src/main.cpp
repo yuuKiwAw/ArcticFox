8ba0b253c2fd32e048022ca2c0fe7363f3fbe612
yuuKiwAw <zhangtuo1999@gmail.com>
2023-03-14 16:51:01 +0800

    test(ArcticFox): update example code

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,15 +1,27 @@
 #include "Application.hpp"
 #include "EntryPoint.hpp"
+#include "Input/Input.hpp"
 
 class ExampleLayer : public ArcticFox::Layer
 {
 public:
+	virtual void OnUpdate(float ts) override
+	{
+		if (ArcticFox::Input::IsKeyDown(ArcticFox::KeyCode::Escape)) {
+			printf("Pressed Espace\n");
+		}
+	}
+
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Hello ArcticFox");
+		// ImGui::Text("FPS: %d", ArcticFox::Application::Get().GetFPS());
+		// ImGui::Text("FPS:%.1f", ImGui::GetIO().Framerate);
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 		if (ImGui::Button("Button")) {
 
         }
+		ImGui::ShowDemoWindow();
 		ImGui::End();
 	}
 };
@@ -28,7 +40,12 @@ public:
 };
 
 ArcticFox::Application* ArcticFox::CreateApplication(int argc, char** argv) {
-	ArcticFox::Application* app = new ArcticFox::Application();
+	ArcticFox::ApplicationSpecification appSpec;
+	appSpec.Name = "ArcticFox Example";
+	appSpec.Width = 800;
+	appSpec.Height = 600;
+
+	ArcticFox::Application* app = new ArcticFox::Application(appSpec);
     app->PushLayer<ExampleLayer>();
     app->PushLayer<ExampleLayer2>();
 
